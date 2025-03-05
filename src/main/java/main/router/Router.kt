25f1aad9343084b7b5a2a046c46aafa34a067505
Jadiefication <jadeczjade008@gmail.com>
2025-03-05 22:46:16 +0100

    fix: made router check for malformed routes

--- src/main/java/main/router/Router.kt
+++ src/main/java/main/router/Router.kt
@@ -6,6 +6,7 @@ import main.java.main.DTO.RequestDTO
 import main.java.main.DTO.ResponseDTO
 import main.java.main.HTTP.Builder.HTTPBuilder
 import main.router.exceptions.NotAnnotatedException
+import main.router.exceptions.RouteNoTargetException
 import main.router.exceptions.RouteTargetUsedException
 import java.net.Socket
 import kotlin.reflect.full.findAnnotation
@@ -24,7 +25,11 @@ class Router {
                 if (routes.containsKey(annotation.target)) {
                     throw RouteTargetUsedException()
                 } else {
-                    routes[annotation.target] = route
+                    if (annotation.target.startsWith("/")) {
+                        routes[annotation.target] = route
+                    } else {
+                        throw RouteNoTargetException()
+                    }
                 }
             } else {
                 throw NotAnnotatedException()
--- src/main/java/main/router/exceptions/RouteNoLocationException.kt
+++
@@ -1,4 +0,0 @@
-package main.router.exceptions
-
-class RouteNoLocationException: Exception("Route has no specified html/target location") {
-}
