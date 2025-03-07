23953d1351dcdf444e63547ae4a6c8c4f0269e73
Jadiefication <jadeczjade008@gmail.com>
2025-03-07 20:28:25 +0100

    refactor: redo the whole element system

--- src/main/java/main/test/routes/home/HomeRoute.kt
+++ src/main/java/main/test/routes/home/HomeRoute.kt
@@ -4,23 +4,75 @@ import main.html.attributes.AttributeNames
 import main.html.attributes.AttributeTypes
 import main.html.attributes.attribute
 import main.html.element.Element
-import main.html.element.content.Div
-import main.html.element.content.HtmlString
+import main.html.element.content.*
 import main.html.element.content.formatting.Br
-import main.html.element.content.H1
+import main.html.element.content.formatting.Hr
 import main.html.page.Page
+import java.net.URL
 
-val divAtt = attribute {
-    name = AttributeNames.CLASS
-    value = "test"
-}
+class HomeRoute : Page(target = "/") {
+    override var content: Element? = Div(
+        attribute {
+            name = AttributeNames.CLASS
+            value = "main-container"
+        },
+        attribute {
+            name = AttributeNames.ID
+            value = "content"
+        }
+    ) {
+        text<H1>(
+            attribute = arrayOf(),
+            type = H1(),
+            text = HtmlString.fromSinglePositions(
+            mutableMapOf(6 to Br()),
+            "Main Title"
+        ))
 
-class HomeRoute: Page(
-    target = "/"
-) {
+        selfClosingElement<Hr>(
+            type = Hr()
+        )
 
-    override var content: Element? = Div(divAtt) {
-        textElement<H1>(HtmlString.fromSinglePositions(mutableMapOf(5 to Br()), "Hello, How are you"))
-        selfClosingElement<Br>()
+        div(
+            attribute {
+                name = AttributeNames.CLASS
+                value = "section"
+            }
+        ) {
+            text<H2>(
+                attribute = arrayOf(),
+                type = H2(),
+                text = HtmlString.fromRanges(
+                mutableMapOf(
+                    IntRange(0, 13) to A(
+                        attribute {
+                            name = AttributeNames.HREF
+                            value = URL("https://example.com").toString()
+                        },
+                        attribute {
+                            name = AttributeNames.TARGET
+                            value = "_blank"
+                        },
+                        attribute {
+                            name = AttributeNames.REL
+                            value = "noopener"
+                        }
+                    ) { }
+                ),
+                "Click Me Title"
+            ))
+
+            selfClosingElement<Br>(
+                type = Br()
+            )
+
+            text<H3>(
+                attribute = arrayOf(),
+                type = H3(),
+                text = HtmlString.fromSinglePositions(
+                mutableMapOf(),
+                "Subtitle Here"
+            ))
+        }
     }
 }
