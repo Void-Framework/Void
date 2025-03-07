dc418b1692e3e0be006ecc6c42eca2e7a57f2fe3
Jadiefication <jadeczjade008@gmail.com>
2025-03-07 20:12:47 +0100

    feat: add attributes to HElement

--- src/main/java/main/html/element/content/HElements.kt
+++ src/main/java/main/html/element/content/HElements.kt
@@ -1,24 +1,50 @@
 package main.html.element.content
 
+import main.html.attributes.Attribute
 import main.html.attributes.AttributeNames
 import main.html.element.TextElement
 
+interface HElement
 
-class H1(text: HtmlString? = null): TextElement("h1", text = text) {
+class H1(text: HtmlString? = null, vararg attributes: Attribute): TextElement("h1", text = text), HElement {
     override val allowedAttributes: List<AttributeNames> = listOf()
+
+    init {
+        addAttributes(*attributes)
+    }
 }
-class H2(text: HtmlString? = null) : TextElement("h2", text = text) {
+class H2(text: HtmlString? = null, vararg attributes: Attribute) : TextElement("h2", text = text), HElement  {
     override val allowedAttributes: List<AttributeNames> = listOf()
+
+    init {
+        addAttributes(*attributes)
+    }
 }
-class H3(text: HtmlString? = null) : TextElement("h3", text = text) {
+class H3(text: HtmlString? = null, vararg attributes: Attribute) : TextElement("h3", text = text), HElement  {
     override val allowedAttributes: List<AttributeNames> = listOf()
+
+    init {
+        addAttributes(*attributes)
+    }
 }
-class H4(text: HtmlString? = null) : TextElement("h4", text =  text) {
+class H4(text: HtmlString? = null, vararg attributes: Attribute) : TextElement("h4", text =  text), HElement  {
     override val allowedAttributes: List<AttributeNames> = listOf()
+
+    init {
+        addAttributes(*attributes)
+    }
 }
-class H5(text: HtmlString? = null) : TextElement("h5", text =  text) {
+class H5(text: HtmlString? = null, vararg attributes: Attribute) : TextElement("h5", text =  text), HElement  {
     override val allowedAttributes: List<AttributeNames> = listOf()
+
+    init {
+        addAttributes(*attributes)
+    }
 }
-class H6(text: HtmlString? = null) : TextElement("h6", text =  text) {
+class H6(text: HtmlString? = null, vararg attributes: Attribute) : TextElement("h6", text =  text), HElement  {
     override val allowedAttributes: List<AttributeNames> = listOf()
+
+    init {
+        addAttributes(*attributes)
+    }
 }
