ade40bb3a97c169ec840c0ecf48e99b4aa2f949c
Jadiefication <jadeczjade008@gmail.com>
2025-03-07 17:44:23 +0100

    refactor: made serverGetter run sync

--- src/main/java/main/router/Router.kt
+++ src/main/java/main/router/Router.kt
@@ -9,11 +9,14 @@ import main.java.main.HTTP.Builder.HTTPBuilder
 import main.router.exceptions.RouteNoTargetException
 import main.router.exceptions.RouteTargetUsedException
 import java.net.Socket
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
 
 class Router {
 
     private val routes: ConcurrentHashMap<String, Page> = ConcurrentHashMap()
+    private val builder = HTTPBuilder()
 
     //Add a function to add routes without finding the annotations
     fun addRoute(route: Page): Router {
@@ -40,13 +43,11 @@ class Router {
     fun route(requestDTO: RequestDTO, client: Socket) {
         val target = requestDTO.target
         if (routes.containsKey(target)) {
-            if (routes[target] is ApiPage) {
-                HTTPBuilder().build(
-                    (routes[target] as ApiPage).serverGetter(request = requestDTO),
-                    client.getOutputStream()
-                )
+            val page = routes[target]
+            if (page is ApiPage) {
+                builder.build(page.serverGetter(request = requestDTO), client.getOutputStream())
             } else {
-                HTTPBuilder().build(
+                builder.build(
                     response = ResponseDTO(
                         status = 200,
                         statusText = "All is well",
@@ -55,13 +56,13 @@ class Router {
                             "Upgrade" to "websocket",
                             "Connection" to "Upgrade"
                         ),
-                        body = "<html><body>${routes[target]!!.content!!.render()}</body></html>"
+                        body = "<html><body>${page!!.content!!.render()}</body></html>"
                     ),
                     outputStream = client.getOutputStream()
                 )
             }
         } else {
-            HTTPBuilder().build(
+            builder.build(
                 response = ResponseDTO(status = 404,
                 statusText = "Not Found",
                 headers = mapOf(
@@ -75,7 +76,7 @@ class Router {
     }
 
     fun error(client: Socket, e: Exception) {
-        HTTPBuilder().build(
+        builder.build(
             response = ResponseDTO(status = 500,
                 statusText = "Server Error",
                 headers = mapOf(
