83da8fc8663b1f27b76b02da81823e0732969f27
Jadiefication <jadeczjade008@gmail.com>
2025-03-09 21:44:22 +0100

    feat: add way to deny HTTP/HTTPS requests depending on Server

--- main/src/main/kotlin/io/void/http/parser/HTTPParser.kt
+++ main/src/main/kotlin/io/void/http/parser/HTTPParser.kt
@@ -2,9 +2,14 @@ package io.void.http.parser
 
 import io.void.api.method.Method
 import io.void.dto.RequestDTO
+import io.void.dto.ResponseDTO
+import io.void.http.builder.HTTPBuilder
+import io.void.router.Router
+import io.void.server.Server
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.net.Socket
 import java.util.*
 
 
@@ -14,11 +19,20 @@ class HTTPParser {
     private lateinit var method: Method
     private lateinit var path: String
 
-    fun parse(inputSteam: InputStream): RequestDTO {
-        val reader = BufferedReader(InputStreamReader(inputSteam))
-        var line = reader.readLine()?.split(" ") ?: throw IllegalStateException("Empty request received")
-        if (line.size < 2) throw IllegalArgumentException("Invalid request line")
-        method = Method.valueOf(line[0].uppercase(Locale.getDefault()))
+    fun parse(inputStream: InputStream, client: Socket, server: Server): RequestDTO {
+        if (checkForClientAndServerHTTPS(
+            client = client,
+            server = server
+        )) return RequestDTO(Method.GET, "/", headers, "") // Provide a default request
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val line = reader.readLine()?.split(" ") ?: throw IllegalStateException("Empty request received")
+        try {
+            if (line.size < 2) throw IllegalArgumentException("Invalid request line")
+            method = Method.valueOf(line[0].uppercase(Locale.getDefault()))
+        } catch (e: Exception) {
+            Router().error(client, e)
+            return RequestDTO(Method.GET, "/", headers, "") // Provide a default request
+        }
         path = line[1]
 
         var headerLine: String?
@@ -38,5 +52,31 @@ class HTTPParser {
         val requestDTO = RequestDTO(method, path, headers, body.toString())
 
         return requestDTO
+    }
+
+    private fun checkForClientAndServerHTTPS(client: Socket, server: Server): Boolean {
+        val inputStream = client.getInputStream()
+        val sssStream = inputStream.mark(5)
+        val firstBytes = ByteArray(5)
+
+        inputStream.read(firstBytes)
+        inputStream.reset()
+
+        val isHTTPS = firstBytes[0] == 0x16.toByte()
+
+        if (isHTTPS && server.isHTTP) {
+            HTTPBuilder().build(
+                ResponseDTO(
+                    status = 301,
+                    statusText = "Moved Permamently",
+                    headers = mutableMapOf(),
+                    body = "Location: http://${client.inetAddress.hostAddress}:${server.port}"
+                ),
+                client.getOutputStream()
+            )
+            return true
+        } else {
+            return false
+        }
     }
 }
