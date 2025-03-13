c4f9a01c9d573e48fa7accdcaada4c703ba04740
Jadiefication <jadeczjade008@gmail.com>
2025-03-13 19:25:09 +0100

    fix: fix incorrect imports

--- main/src/main/kotlin/io/void/html/exceptions/ChildNotAllowedException.kt
+++ main/src/main/kotlin/io/void/html/exceptions/ChildNotAllowedException.kt
@@ -1,5 +1,5 @@
 package io.void.html.exceptions
 
-import io.void.html.element.Element
+import io.void.html.Element
 
 class ChildNotAllowedException(child: Element, parent: Element): Exception("${parent.name} doesn't take ${child.name} as it's child element.")
