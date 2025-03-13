8838035443fd6fb41f012618809e3c93c2bbc5f7
Jadiefication <jadeczjade008@gmail.com>
2025-03-13 18:07:14 +0100

    feat: add child handling in ElementGen.kt

--- main/src/main/kotlin/io/void/generator/ElementGen.kt
+++ main/src/main/kotlin/io/void/generator/ElementGen.kt
@@ -34,7 +34,7 @@ fun main(args: Array<String>) {
     }
 
     processedLines.forEach { line ->
-        val kotlinCode = StringBuilder("package io.void.generated\n\nimport io.void.html.attributes.Attribute\nimport io.void.html.attributes.AttributeNames\n")
+        val kotlinCode = StringBuilder("package io.void.generated\n\nimport io.void.html.attributes.Attribute\nimport io.void.html.attributes.AttributeNames\nimport io.void.generated.*\nimport kotlin.reflect.KClass\n")
         val startLength = kotlinCode.length
         val attributeBuilder = StringBuilder("")
         val name = line.substringBefore("\":").substringAfter("\"").capitalize()
@@ -43,26 +43,44 @@ fun main(args: Array<String>) {
         if (attributes.isNotEmpty()) {
             attributes.forEach { attribute ->
                 if (attribute.isNotBlank()) {
-                    attributeBuilder.append("AttributeNames.$attribute,")
+                    attributeBuilder.append("AttributeNames.$attribute, ")
                 }
             }
             if (attributeBuilder.isNotEmpty()) {
-                attributeBuilder.setLength(attributeBuilder.length - 1)
+                attributeBuilder.setLength(attributeBuilder.length - 2)
             }
         }
         when (type) {
             "Normal" -> {
                 kotlinCode.append("\nclass $name(vararg attributes: Attribute, function: Element.() -> Unit): ElementWithChildren(name = \"${name.lowercase()}\") {\n")
                 kotlinCode.insert(startLength, "import io.void.html.element.Element\nimport io.void.html.element.ElementWithChildren\n")
+                val childrenBuilder = StringBuilder("null")
+                val acceptedChildren = line.substringAfter("\"acceptedChildren\": [").substringBefore("]").split(", ").map { it.replace("\"", "") }.toList()
+                if (acceptedChildren.isNotEmpty()) {
+                    if (acceptedChildren[0].isNotBlank()) {
+                        childrenBuilder.setLength(0)
+                    }
+                    acceptedChildren.forEach { child ->
+                        if (child.isNotBlank()) {
+                            childrenBuilder.append("${child.capitalize()}::class, ")
+                        }
+                    }
+                    if (acceptedChildren[0].isNotBlank()) {
+                        childrenBuilder.setLength(childrenBuilder.length - 2)
+                    }
+                }
+                kotlinCode.append("    override val acceptedChildren: MutableList<KClass<out Element>?> = mutableListOf($childrenBuilder)\n\n")
             }
             "Void" -> {
                 kotlinCode.append("\nclass $name(vararg attribute: Attribute): SelfClosingElement(\"${name.lowercase()}\") {\n")
                 kotlinCode.insert(startLength, "import io.void.html.element.SelfClosingElement\n")
+                kotlinCode.append("\n    init {\n        addAttributes(*attribute)\n    }\n\n")
             }
             else -> throw UnsupportedOperationException()
         }
         kotlinCode.append("    override val allowedAttributes: List<AttributeNames> = listOf($attributeBuilder)\n")
 
+        kotlinCode.append("}")
         println(kotlinCode)
     }
 }
