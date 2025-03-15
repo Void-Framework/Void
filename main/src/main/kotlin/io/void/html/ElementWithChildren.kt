c489c6314ea1b21ccb342932a63fe966f88834a9
Jadiefication <jadeczjade008@gmail.com>
2025-03-15 20:11:18 +0100

    refactor: add specific error for fragment children

--- main/src/main/kotlin/io/void/html/ElementWithChildren.kt
+++ main/src/main/kotlin/io/void/html/ElementWithChildren.kt
@@ -1,6 +1,7 @@
 package io.void.html
 
 import io.void.html.exceptions.ChildNotAllowedException
+import io.void.html.exceptions.FragmentChildNotAllowedException
 import kotlin.reflect.KClass
 
 internal interface HElement
@@ -17,17 +18,24 @@ abstract class ElementWithChildren internal constructor(override val name: Strin
         /**
          * If the first value in the array is null it means the element can accept any children.
          *
-         * Else if you added in a child that is not in the accepted list, the ChildNotAllowedException will be thrown
+         * Else if you added in a child that is not in the accepted list, the ChildNotAllowedException will be thrown.
+         * Optionally if the child is a fragment and one of the first set of children doesn't is not in the accepted list, the FragmentChildNotAllowedException will be thrown.
          *
          * @throws ChildNotAllowedException
          */
         if (acceptedChildren[0] != null) {
             children?.forEach { child ->
                 if (!isAccepted(child)) {
-                    throw ChildNotAllowedException(
-                        child = child,
-                        parent = this
-                    )
+                    if (child is Fragment) {
+                        throw FragmentChildNotAllowedException(
+                            parent = this
+                        )
+                    } else {
+                        throw ChildNotAllowedException(
+                            child = child,
+                            parent = this
+                        )
+                    }
                 }
             }
         }
--- main/src/main/kotlin/io/void/html/exceptions/ChildNotAllowedException.kt
+++ main/src/main/kotlin/io/void/html/exceptions/ChildNotAllowedException.kt
@@ -3,3 +3,4 @@ package io.void.html.exceptions
 import io.void.html.Element
 
 class ChildNotAllowedException(child: Element, parent: Element): Exception("${parent.name} doesn't take ${child.name} as it's child element.")
+class FragmentChildNotAllowedException(parent: Element): Exception("${parent.name} doesn't take one of the Fragment's child as it's child element.")
