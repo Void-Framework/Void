226372d32dbce0aaa19a82290dd9f02c030276ad
Jadiefication <jadeczjade008@gmail.com>
2025-03-15 18:16:52 +0100

    fix: fix home route for tests

--- test/src/main/kotlin/io/jadiefication/routes/home/HomeRoute.kt
+++ test/src/main/kotlin/io/jadiefication/routes/home/HomeRoute.kt
@@ -1,10 +1,8 @@
 package io.jadiefication.routes.home
 
-import io.void.generated.A
-import io.void.generated.Br
-import io.void.generated.Div
-import io.void.generated.Hr
+import io.void.generated.*
 import io.void.html.Element
+import io.void.html.Fragment
 import io.void.html.attributes.AttributeNames
 import io.void.html.attributes.attribute
 import io.void.html.page.Page
@@ -21,54 +19,43 @@ class HomeRoute : Page(target = "/") {
             value = "content"
         }
     ) {
-        text<H1>(
-            attribute = arrayOf(),
-            type = H1(),
-            text = "Main${Br().render()} Title"
-        )
+        H1 {
+            Fragment("Main${Br().render()} Title")
+        }
 
-        selfClosingElement<Hr>(
-            type = Hr()
-        )
+        Hr()
 
-        div(
-            attribute {
+        Div(
+            attribute = arrayOf(attribute {
                 name = AttributeNames.CLASS
                 value = "section"
-            }
+            }),
         ) {
-            text<H2>(
-                attribute = arrayOf(),
-                type = H2(),
-                text = "${
-                    A(
-                    attribute {
-                        name = AttributeNames.HREF
-                        value = URL("https://example.com")
-                    },
-                    attribute {
-                        name = AttributeNames.TARGET
-                        value = "_blank"
-                    },
-                    attribute {
-                        name = AttributeNames.REL
-                        value = "noopener"
-                    },
-                    function = { }
-                ).render()}Click Me Title"
-            )
+            H2 {
+                A(
+                    attribute = arrayOf(
+                        attribute {
+                            name = AttributeNames.HREF
+                            value = URL("https://example.com")
+                        },
+                        attribute {
+                            name = AttributeNames.TARGET
+                            value = "_blank"
+                        },
+                        attribute {
+                            name = AttributeNames.REL
+                            value = "noopener"
+                        }),
+                ) {
+                    Fragment(_text = "Click Me Title")
+                }
+            }
 
-            br(
-                type = Br()
-            )
+            Br()
 
-            this.
-
-            text<H3>(
-                attribute = arrayOf(),
-                type = H3(),
-                text = "Subtitle Here"
-            )
+           H3 {
+               Fragment("Subtitle Here")
+           }
         }
     }
 }
