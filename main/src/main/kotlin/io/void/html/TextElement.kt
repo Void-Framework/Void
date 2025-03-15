ea5f6facd36fb5573ba2d00414e2dcfba46e6b2c
Jadiefication <jadeczjade008@gmail.com>
2025-03-15 16:48:04 +0100

    refactor: deprecated TextElement

--- main/src/main/kotlin/io/void/html/TextElement.kt
+++ main/src/main/kotlin/io/void/html/TextElement.kt
@@ -1,5 +1,13 @@
 package io.void.html
 
+@Deprecated(
+    message = "No need for this class as of the moment, might be deleted in future commits.",
+    replaceWith = ReplaceWith(
+        expression = "Use ElementWithChildren, since most of the supported elements support both text and child elements.",
+        imports = arrayOf("ElementWithChildren")
+    ),
+    level = DeprecationLevel.HIDDEN
+)
 abstract class TextElement internal constructor(name: String, private var text: String?) : Element(name) {
 
     override fun render(): String {
