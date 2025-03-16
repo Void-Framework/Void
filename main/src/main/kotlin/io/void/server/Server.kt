54a4ee8463d1033c100069c24592358b77d28edd
Jadiefication <jadeczjade008@gmail.com>
2025-03-16 18:42:08 +0100

    feat: add way to auto redirect to HTTPS

--- main/src/main/kotlin/io/void/server/Server.kt
+++ main/src/main/kotlin/io/void/server/Server.kt
@@ -1,7 +1,10 @@
 package io.void.server
 
 import io.void.clienthandler.ClientHandler
+import io.void.dto.ResponseDTO
+import io.void.http.builder.HTTPBuilder
 import io.void.router.Router
+import io.void.server.exception.HTTPSNotOnException
 import java.io.File
 import java.io.FileInputStream
 import java.net.ServerSocket
@@ -14,24 +17,42 @@ import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLServerSocket
 import javax.net.ssl.SSLSocket
 
-class Server(private val router: Router, val port: Int) {
+class Server(private val router: Router) {
 
     private lateinit var socket: ServerSocket
     private val executorService: ExecutorService = Executors.newCachedThreadPool()
     private val keystore: KeyStore = KeyStore.getInstance("PKCS12")
     private val context: SSLContext = SSLContext.getInstance("TLS")
+    private var isHTTPSOn = false
+    private val builder: HTTPBuilder = HTTPBuilder()
 
-    fun startHTTPServer() {
+    fun startHTTPServer(port: Int, routeToHTTPS: Boolean) {
         Thread {
             try {
                 socket = ServerSocket(port)
                 while (socket.isBound) {
                     val client = socket.accept()
-                    executorService.submit {
-                        try {
-                            ClientHandler(client).setRouter(router = this.router).start()
-                        } catch (e: Exception) {
-                            ClientHandler(client).error(e)
+                    if (routeToHTTPS) {
+                        if (isHTTPSOn) {
+                            builder.build(
+                                response = ResponseDTO(
+                                    status = 301,
+                                    statusText = "Moved Permanently",
+                                    headers = mutableMapOf("Location" to "https://${client.inetAddress.hostName}"),
+                                    body = ""
+                                ),
+                                outputStream = client.getOutputStream()
+                            )
+                        } else {
+                            throw HTTPSNotOnException()
+                        }
+                    } else {
+                        executorService.submit {
+                            try {
+                                ClientHandler(client).setRouter(router = this.router).start()
+                            } catch (e: Exception) {
+                                ClientHandler(client).error(e)
+                            }
                         }
                     }
                 }
@@ -43,7 +64,7 @@ class Server(private val router: Router, val port: Int) {
         }.start()
     }
 
-    fun startHTTPSServer(password: String, file: File, needsAuth: Boolean) {
+    fun startHTTPSServer(port: Int, password: String, file: File, needsAuth: Boolean) {
         val paswd = password.toCharArray()
         try {
             FileInputStream(file).use { keystore.load(it, paswd) }
@@ -55,6 +76,7 @@ class Server(private val router: Router, val port: Int) {
             val factory = context.serverSocketFactory
 
             val server = factory.createServerSocket(port) as SSLServerSocket
+            isHTTPSOn = true
             server.needClientAuth = needsAuth
             while (server.isBound) {
                 val client = server.accept() as SSLSocket
