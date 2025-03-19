eb3014ff86698350b0efd752a1ff7e07a29e3f91
Jadiefication <jadeczjade008@gmail.com>
2025-03-19 17:55:22 +0100

    feat: add differentiation of WSClient

--- main/src/main/kotlin/io/void/ws/WSServer.kt
+++ main/src/main/kotlin/io/void/ws/WSServer.kt
@@ -4,8 +4,19 @@ import org.java_websocket.WebSocket
 import org.java_websocket.handshake.ClientHandshake
 import org.java_websocket.server.WebSocketServer
 import java.net.InetSocketAddress
+import java.util.*
 
 class WSServer(port: Int): WebSocketServer(InetSocketAddress(port)) {
+
+    private val clients = mutableMapOf<UUID, WSClient>()
+
+    private fun registerClient(uuid: UUID, client: WSClient) {
+        clients[uuid] = client
+    }
+
+    fun getClient(uuid: UUID): WSClient? {
+        return clients[uuid]
+    }
 
     override fun onStart() {
         println("Started")
@@ -21,6 +32,13 @@ class WSServer(port: Int): WebSocketServer(InetSocketAddress(port)) {
 
     override fun onMessage(p0: WebSocket?, p1: String?) {
         p0?.send("Server received: $p1")
+        if (p1 != null && p1.startsWith("REGISTER:")) {
+            val uuid = UUID.fromString(p1.substringAfter("REGISTER:"))
+            registerClient(
+                uuid = uuid,
+                client = p0 as WSClient
+            )
+        }
         println("Message: $p1")
     }
 
