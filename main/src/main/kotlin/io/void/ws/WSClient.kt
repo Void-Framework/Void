8e631575c7f3c6b524c70369657f58d4ca8a7c46
Jadiefication <jadeczjade008@gmail.com>
2025-03-19 17:55:22 +0100

    feat: add differentiation of WSClient

--- main/src/main/kotlin/io/void/ws/WSClient.kt
+++ main/src/main/kotlin/io/void/ws/WSClient.kt
@@ -3,11 +3,19 @@ package io.void.ws
 import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
 import java.net.URI
+import java.util.UUID
 
 class WSClient(uri: URI): WebSocketClient(uri) {
 
+    private var uuid: UUID
+
+    init {
+        uuid = UUID.randomUUID()
+    }
+
     override fun onOpen(p0: ServerHandshake?) {
         println("Connected to server")
+        send("REGISTER:$uuid")
     }
 
     override fun onClose(p0: Int, p1: String?, p2: Boolean) {
