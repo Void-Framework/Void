a9a191f07d6f658b702b2d52a9e6bc6738974b61
Jadiefication <jadeczjade008@gmail.com>
2025-03-19 19:13:04 +0100

    fix!: fix DataHolder unable to initialize

--- main/src/main/kotlin/io/void/html/page/Page.kt
+++ main/src/main/kotlin/io/void/html/page/Page.kt
@@ -1,10 +1,35 @@
 package io.void.html.page
 
 import io.void.html.Element
+import io.void.js.EventDispatcher
+import io.void.js.data.DataHolder
 import io.void.ws.WSClient
 
 abstract class Page(open val target: String) {
 
     abstract var content: Element?
-    internal lateinit var client: WSClient
+    var client: WSClient? = null
+        set(value) {
+            field = value
+            // Update all DataHolder instances with the new client
+            updateDataHolders(content, value)
+        }
+
+    private fun updateDataHolders(element: Element?, client: WSClient?) {
+        if (element == null) return
+
+        if (element is DataHolder<*>) {
+            element.setClient(client!!)
+        }
+
+        element.children?.forEach { child ->
+            updateDataHolders(child, client)
+        }
+    }
+
+    init {
+        EventDispatcher.addEventListener(DataHolder::class.java) {
+            println(it.get())
+        }
+    }
 }
