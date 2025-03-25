1460bc9210b31741fbf7f6a00e0f8abf0501f1f7
Jadiefication <jadeczjade008@gmail.com>
2025-03-25 22:06:26 +0100

    refactor: move attribute processing out

--- main/src/main/kotlin/io/void/html/page/Page.kt
+++ main/src/main/kotlin/io/void/html/page/Page.kt
@@ -8,25 +8,12 @@ import io.void.js.data.DataHolder
 abstract class Page(open val target: String) {
 
     abstract var content: Element?
-    private val classAttributes: MutableMap<Element, List<String>> = mutableMapOf()
+    val classAttributes: MutableMap<Element, List<String>> = mutableMapOf()
 
     init {
-        if (content != null) {
-            if (content!!.attributes.containsKey(AttributeNames.CLASS)) {
-                putInTailwind(content!!)
-            }
-            content!!.children?.forEach {
-                putInTailwind(it)
-            }
-        }
         EventDispatcher.addEventListener(DataHolder::class.java) {
             println(it.get())
         }
     }
 
-    private fun putInTailwind(element: Element) {
-        if (element.attributes.containsKey(AttributeNames.CLASS)) {
-            classAttributes[element] = element.attributes[AttributeNames.CLASS]!!.split(" ")
-        }
-    }
 }
