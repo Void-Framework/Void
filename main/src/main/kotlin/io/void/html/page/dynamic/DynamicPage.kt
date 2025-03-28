c403e9d9c3c6cd20602a7c51acb4e8cd65b24037
Jadiefication <jadeczjade008@gmail.com>
2025-03-28 23:20:24 +0100

    feat: add DynamicPage

---
+++ main/src/main/kotlin/io/void/html/page/dynamic/DynamicPage.kt
@@ -0,0 +1,24 @@
+package io.void.html.page.dynamic
+
+import io.void.dto.RequestDTO
+import io.void.generated.S
+import io.void.html.page.Page
+import io.void.html.page.content.ContentType
+import io.void.html.page.metadata.Metadata
+
+/**
+ * Dynamic targets are initialized using this {a}
+ *
+ * Example: /web/test/{a}
+ *
+ */
+abstract class DynamicPage<T : ContentType>(target: String): Page<T>(target = target) {
+
+    internal fun check(requestDTO: RequestDTO): String? {
+        return if (requestDTO.target.contains(target.substringAfter("{"))) {
+            requestDTO.target.substringAfter(target.substringAfter("{"))
+        } else {
+            null
+        }
+    }
+}
