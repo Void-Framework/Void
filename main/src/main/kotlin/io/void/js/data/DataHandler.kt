5ca1fe4d143aafed702c183226c4642e4b970d27
Jadiefication <jadeczjade008@gmail.com>
2025-03-30 20:23:04 +0200

    feat: add function binding to DataHandler

--- main/src/main/kotlin/io/void/js/data/DataHandler.kt
+++ main/src/main/kotlin/io/void/js/data/DataHandler.kt
@@ -1,6 +1,11 @@
 package io.void.js.data
 
 import io.void.js.EventDispatcher
+import io.void.js.JavaScript
+import io.void.js.keywords.Function
+import io.void.js.keywords.Return
+import io.void.js.keywords.function
+import java.util.UUID
 
 class DataHandler private constructor() : IDataHandler {
 
@@ -8,15 +13,22 @@ class DataHandler private constructor() : IDataHandler {
         val singleton = DataHandler()
     }
 
-    override fun <T> create(value: T): DataHolder<T> {
+    override fun <T> create(value: T, js: JavaScript): Pair<DataHolder<T>, Function> {
         return DataHolder(
             value = value,
-        )
+        ) to js.function(randomString(5), listOf("newValue")) {
+            Return(_value = "newValue")
+        }
     }
 
     override fun <T> update(newData: DataHolder<T>) {
         EventDispatcher.callEvent(DataHolder::class.java, newData)
     }
 
-
+    private fun randomString(length: Int): String {
+        val chars = ('a'..'z') + ('A'..'Z')
+        return (1..length)
+            .map { chars.random() }
+            .joinToString("")
+    }
 }
--- main/src/main/kotlin/io/void/js/data/IDataHandler.kt
+++ main/src/main/kotlin/io/void/js/data/IDataHandler.kt
@@ -1,8 +1,11 @@
 package io.void.js.data
 
+import io.void.js.JavaScript
+import io.void.js.keywords.Function
+
 
 interface IDataHandler {
 
-    fun<T> create(value: T): DataHolder<T>
+    fun<T> create(value: T, js: JavaScript): Pair<DataHolder<T>, Function>
     fun<T> update(newData: DataHolder<T>)
 }
