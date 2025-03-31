dfb27889b612742e5cdaf6c23e022c441c9c5eb4
Jadiefication <jadeczjade008@gmail.com>
2025-03-31 19:10:26 +0200

    feat: add calls

---
+++ main/src/main/kotlin/io/void/js/keywords/Call.kt
@@ -0,0 +1,14 @@
+package io.void.js.keywords
+
+data class Call<T: Keyword>(val variableName: String, val callableMethod: T.() -> Any, val clazz: T): Keyword {
+
+    init {
+        clazz.callableMethod()
+    }
+
+    override var jsReturn: String = "$variableName.${clazz.jsReturn}"
+
+    override fun render(): String {
+        return jsReturn
+    }
+}
