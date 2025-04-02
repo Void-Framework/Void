8e91fe45e315f37aa704a540673610e9022a66e9
Jadiefication <jadeczjade008@gmail.com>
2025-04-02 20:32:18 +0200

    feat: add way to call unimplemented functions

--- main/src/main/kotlin/io/void/js/keywords/Call.kt
+++ main/src/main/kotlin/io/void/js/keywords/Call.kt
@@ -1,17 +1,22 @@
 package io.void.js.keywords
 
-data class Call<T: Keyword>(val variableName: String, val callableMethod: T.() -> Any, val clazz: T): Keyword {
+data class Call<T: Keyword> internal constructor(val variableName: String): Keyword {
 
-    init {
+    constructor(variableName: String, callableFunction: String): this(variableName) {
+        jsReturn = "$variableName.$callableFunction;"
+    }
+
+    constructor(variableName: String, callableMethod: T.() -> Any, clazz: T): this(variableName) {
         clazz.callableMethod()
+        jsReturn = "$variableName${if (clazz.jsReturn.startsWith(".")){
+            ""
+        } else {
+            "."
+        }
+        }${clazz.jsReturn}"
     }
 
-    override var jsReturn: String = "$variableName${if (clazz.jsReturn.startsWith(".")){
-        ""
-    } else {
-        "."
-    }
-    }${clazz.jsReturn}"
+    override var jsReturn = ""
 
     override fun render(): String {
         return jsReturn
