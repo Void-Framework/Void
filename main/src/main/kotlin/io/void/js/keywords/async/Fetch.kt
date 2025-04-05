d34605be5eb1bba40ad783fb5362b56fee73fcd0
Jadiefication <jadeczjade008@gmail.com>
2025-04-05 15:00:42 +0200

    feat: add fetch API

---
+++ main/src/main/kotlin/io/void/js/keywords/async/Fetch.kt
@@ -0,0 +1,73 @@
+package io.void.js.keywords.async
+
+import io.void.dto.ResponseDTO
+import io.void.js.JavaScript
+import io.void.js.exception.JsSyntaxException
+import io.void.js.keywords.Function
+import io.void.js.keywords.Keyword
+import java.net.URL
+
+data class FetchFunction(
+    val _body: JavaScript.(Function) -> Unit,
+    val responseArgName: String
+): Function(
+    name = "",
+    arguments = listOf(responseArgName),
+    body = _body
+) {
+    override var jsReturn: String = "$responseArgName => {${children.joinToString(";") { it.render() }}}"
+
+    override fun render(): String {
+        return jsReturn
+    }
+}
+
+data class Fetch(
+    val data: ResponseDTO?,
+    val url: URL,
+    val inAsyncFunction: Boolean = false
+): Keyword {
+
+    private var headers: String = ""
+
+    override var jsReturn: String = "fetch($url${if (data != null) {
+        ", {method: '${data.statusText}', headers: {$headers}, body: ${data.body}"
+    } else {
+        ""
+    }
+    })"
+
+    init {
+        data?.headers?.forEach { (name, value) ->
+            headers += "'$name': '$value',"
+        }
+        if (data?.headers?.isNotEmpty() == true) {
+            headers.replaceAfterLast(",", "")
+        }
+    }
+
+    override fun render(): String {
+        return "$jsReturn;"
+    }
+
+    fun then(function: FetchFunction): Fetch {
+        if (inAsyncFunction) throw JsSyntaxException(this)
+        jsReturn += ".then(${function.render()})"
+        return this
+    }
+
+    fun catch(function: FetchFunction) {
+        if (inAsyncFunction) throw JsSyntaxException(this)
+        jsReturn += ".catch(${function.render()})"
+    }
+}
+
+fun JavaScript.fetch(data: ResponseDTO?, url: URL, inAsyncFunction: Boolean = false): Fetch {
+    val fetch = Fetch(
+        data= data,
+        url = url,
+        inAsyncFunction = inAsyncFunction
+    )
+    children.add(fetch)
+    return fetch
+}
