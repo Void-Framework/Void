cc52e02b1647fcf74d75b472a1615e74e67299b3
Jadiefication <jadeczjade008@gmail.com>
2025-04-05 11:41:12 +0200

    feat: add loops

---
+++ main/src/main/kotlin/io/void/js/keywords/controlflow/Loop.kt
@@ -0,0 +1,66 @@
+package io.void.js.keywords.controlflow
+
+import io.void.js.JavaScript
+import io.void.js.keywords.Function
+
+data class While(
+    val condition: String,
+    val _body: JavaScript.(Function) -> Unit,
+    val js: JavaScript
+): Function(
+    name = "",
+    arguments = emptyList(),
+    body = _body
+) {
+
+    override var jsReturn: String = "while($condition) {${children.joinToString(";") { it.render() }}}"
+
+    init {
+        _body(js, this)
+    }
+
+    override fun render(): String {
+        return jsReturn
+    }
+}
+
+data class For(
+    val condition: String,
+    val _body: JavaScript.(Function) -> Unit,
+    val js: JavaScript
+): Function(
+    name = "",
+    arguments = emptyList(),
+    body = _body
+) {
+
+    override var jsReturn: String = "for($condition) {${children.joinToString(";") { it.render() }}}"
+
+    init {
+        _body(js, this)
+    }
+
+    override fun render(): String {
+        return jsReturn
+    }
+}
+
+fun JavaScript.While(condition: String, body: JavaScript.(Function) -> Unit): While {
+    val While = While(
+        condition = condition,
+        _body = body,
+        js = this
+    )
+    children.add(While)
+    return While
+}
+
+fun JavaScript.For(condition: String, body: JavaScript.(Function) -> Unit): For {
+    val For = For(
+        condition = condition,
+        _body = body,
+        js = this
+    )
+    children.add(For)
+    return For
+}
