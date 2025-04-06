17d25e8d73860fea4f26fe1491a3ee3e885fdd3d
Jadiefication <jadeczjade008@gmail.com>
2025-04-06 17:42:58 +0200

    fix: fix keywords being improperly set in js values

--- main/src/main/kotlin/io/void/js/keywords/Let.kt
+++ main/src/main/kotlin/io/void/js/keywords/Let.kt
@@ -2,7 +2,12 @@ package io.void.js.keywords
 
 class Let<T>(val value: T, val name: String): Keyword {
 
-    override var jsReturn: String = "let $name = $value"
+    override var jsReturn: String = "let $name = ${if (value is Keyword) {
+        value.render()
+    } else {
+        "$value"
+    }
+    }"
 
     override fun render(): String {
         return jsReturn
@@ -10,7 +15,12 @@ class Let<T>(val value: T, val name: String): Keyword {
 
     inner class Setter(newValue: T): Keyword {
 
-        override var jsReturn: String = "$name = $newValue"
+        override var jsReturn: String = "$name = ${if (value is Keyword) {
+            value.render()
+        } else {
+            "$value"
+        }
+        }"
 
         override fun render(): String {
             return jsReturn
