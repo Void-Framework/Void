9cc44ed508c49f8d62c222377ba2c6f214f95a8f
Jadiefication <jadeczjade008@gmail.com>
2025-04-06 19:25:50 +0200

    feat: add calls outside of functions

--- main/src/main/kotlin/io/void/js/keywords/Call.kt
+++ main/src/main/kotlin/io/void/js/keywords/Call.kt
@@ -1,5 +1,7 @@
 package io.void.js.keywords
 
+import io.void.js.JavaScript
+
 data class Call<T: Keyword> internal constructor(val variableName: String): Keyword {
 
     constructor(variableName: String, callableFunction: String): this(variableName) {
@@ -21,4 +23,23 @@ data class Call<T: Keyword> internal constructor(val variableName: String): Keyw
     override fun render(): String {
         return jsReturn
     }
+}
+
+inline fun <reified T : Keyword> JavaScript.call(variableName: String, callableFunction: String): Call<T> {
+    val call = Call<T>(
+        variableName = variableName,
+        callableFunction = callableFunction
+    )
+    children.add(call)
+    return call
+}
+
+inline fun <reified T : Keyword> JavaScript.call(variableName: String, noinline callableMethod: T.() -> Any, clazz: T): Call<T> {
+    val call = Call(
+        variableName = variableName,
+        callableMethod = callableMethod,
+        clazz = clazz
+    )
+    children.add(call)
+    return call
 }
