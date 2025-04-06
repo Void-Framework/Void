1d2aafb76ee310aea15be3116f95cd241e144c75
Jadiefication <jadeczjade008@gmail.com>
2025-04-06 14:35:09 +0200

    feat: finish JsList

--- main/src/main/kotlin/io/void/js/keywords/datastructures/JsList.kt
+++ main/src/main/kotlin/io/void/js/keywords/datastructures/JsList.kt
@@ -25,6 +25,42 @@ data class JsList<T>(val arguments: List<T>): Keyword {
             jsReturn += ".forEach("
             return Runnable()
         }
+        fun push(item: String) {
+            jsReturn += ".push($item)"
+        }
+        fun pop() {
+            jsReturn += ".pop()"
+        }
+        fun shift() {
+            jsReturn += ".shift()"
+        }
+        fun unshift(item: String) {
+            jsReturn += ".unshift($item)"
+        }
+        fun splice(index: Int, count: Int? = null, item: List<String>?) {
+            jsReturn += ".splice($index${if (count != null) {
+                ", $count"
+            } else {
+                ""
+            }
+            }${if (item != null) {
+                ", ${item.joinToString(", ")}"
+            } else {
+                ""
+            }
+            })"
+        }
+        fun map(): Runnable {
+            jsReturn += ".map("
+            return Runnable()
+        }
+        fun filter(): Runnable {
+            jsReturn += ".filter("
+            return Runnable()
+        }
+        fun includes(item: String) {
+            jsReturn += ".includes($item)"
+        }
     }
 
     inner class Runnable {
@@ -37,6 +73,16 @@ data class JsList<T>(val arguments: List<T>): Keyword {
 
 inline fun <reified T> JavaScript.forEach(list: JsList<T>): JsList<T>.Runnable {
     val runnable = list.Actions().forEach()
+    children.add(list)
+    return runnable
+}
+inline fun <reified T> JavaScript.map(list: JsList<T>): JsList<T>.Runnable {
+    val runnable = list.Actions().map()
+    children.add(list)
+    return runnable
+}
+inline fun <reified T> JavaScript.filter(list: JsList<T>): JsList<T>.Runnable {
+    val runnable = list.Actions().filter()
     children.add(list)
     return runnable
 }
