b339fea3c500e3e364afdd0a5f7b5250371e0b22
Jadiefication <jadeczjade008@gmail.com>
2025-04-08 21:19:28 +0200

    chore: add extension functions

--- main/src/main/kotlin/io/void/js/keywords/error/Error.kt
+++ main/src/main/kotlin/io/void/js/keywords/error/Error.kt
@@ -1,11 +1,12 @@
 package io.void.js.keywords.error
 
+import io.void.js.JavaScript
 import io.void.js.keywords.Keyword
 import io.void.js.keywords.string.TemplateString
 
 data class Error(
     val message: String,
-    val cause: String
+    val extraData: String
 ): Keyword {
 
     override var jsReturn: String = "new Error(${if (TemplateString.isTemplateString(message)) {
@@ -13,9 +14,18 @@ data class Error(
     } else {
         message
     }
-    }$cause)"
+    }$extraData)"
 
     override fun render(): String {
         return jsReturn
     }
+}
+
+fun JavaScript.Error(name: String, extraData: String): Error {
+    val error = Error(
+        message = name,
+        extraData = extraData
+    )
+    children.add(error)
+    return error
 }
