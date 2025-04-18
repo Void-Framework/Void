3e8ce41bd4e43c90b91508e7b96756523650ba7c
Jadiefication <jadeczjade008@gmail.com>
2025-04-18 17:42:01 +0200

    fix: fix semicolons not being added in if/elseif/else

--- main/src/main/kotlin/io/void/js/keywords/controlflow/If.kt
+++ main/src/main/kotlin/io/void/js/keywords/controlflow/If.kt
@@ -2,6 +2,7 @@ package io.void.js.keywords.controlflow
 
 import io.void.js.JavaScript
 import io.void.js.keywords.Function
+import io.void.js.keywords.Keyword
 
 data class If(
     val condition: String,
@@ -17,21 +18,30 @@ data class If(
         _body(js, this)
     }
 
-    override var jsReturn: String = "if($condition) {${children.joinToString(";") { it.render() }}}"
+    private fun renderBlock(statements: List<Keyword>): String {
+        return statements.joinToString(";") { 
+            val rendered = it.render()
+            if (!rendered.endsWith(";")) "$rendered;" else rendered
+        }
+    }
+
+    override var jsReturn: String = "if($condition) {${renderBlock(children)}}"
 
     override fun render(): String {
         return jsReturn
     }
 
     fun ElseIf(condition: String, body: JavaScript.(Function) -> Unit): If {
+        children.clear()
         body(js, this)
-        jsReturn += "else if ($condition) {${children.joinToString(";") { it.render() }}}"
+        jsReturn += "else if ($condition) {${renderBlock(children)}}"
         return this
     }
 
     fun Else(body: JavaScript.(Function) -> Unit): If {
+        children.clear()
         body(js, this)
-        jsReturn += "else {${children.joinToString(";") { it.render() }}}"
+        jsReturn += "else {${renderBlock(children)}}"
         return this
     }
 }
