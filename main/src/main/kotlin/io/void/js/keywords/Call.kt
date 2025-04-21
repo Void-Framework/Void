2474790eaa64a84bdac0b3ff083ad3fbd37a810f
Jadiefication <jadeczjade008@gmail.com>
2025-04-21 17:41:22 +0200

    feat: add functions into calls

--- main/src/main/kotlin/io/void/js/keywords/Call.kt
+++ main/src/main/kotlin/io/void/js/keywords/Call.kt
@@ -3,6 +3,7 @@ package io.void.js.keywords
 import io.void.js.JavaScript
 import io.void.js.keywords.variable.Variable
 
+@ConsistentCopyVisibility
 data class Call<T: Keyword> internal constructor(val variableName: String): Keyword {
 
     override var jsReturn = ""
@@ -35,6 +36,20 @@ data class Call<T: Keyword> internal constructor(val variableName: String): Keyw
         jsReturn = "$variableName.$callableFunction"
     }
 
+    constructor(function: Function, argsList: List<String> = emptyList(), callableMethod: T.() -> Any, clazz: T): this(function.run(argsList)) {
+        clazz.callableMethod()
+        jsReturn = "$variableName${if (clazz.jsReturn.startsWith(".")){
+            ""
+        } else {
+            "."
+        }
+        }${clazz.jsReturn}"
+    }
+
+    constructor(function: Function, argsList: List<String> = emptyList(), callableFunction: String): this(function.run(argsList)) {
+        jsReturn = "$variableName.$callableFunction"
+    }
+
     override fun render(): String {
         return jsReturn
     }
@@ -61,6 +76,27 @@ inline fun <reified T : Keyword> JavaScript.call(variable: Variable<T>, callable
 inline fun <reified T : Keyword> JavaScript.call(variableName: String, noinline callableMethod: T.() -> Any, clazz: T): Call<T> {
     val call = Call(
         variableName = variableName,
+        callableMethod = callableMethod,
+        clazz = clazz
+    )
+    children.add(call)
+    return call
+}
+
+inline fun <reified T : Keyword> JavaScript.call(function: Function, argsList: List<String> = emptyList(), callableFunction: String): Call<T> {
+    val call = Call<T>(
+        function = function,
+        argsList = argsList,
+        callableFunction = callableFunction
+    )
+    children.add(call)
+    return call
+}
+
+inline fun <reified T : Keyword> JavaScript.call(function: Function, argsList: List<String> = emptyList(), noinline callableMethod: T.() -> Any, clazz: T): Call<T> {
+    val call = Call(
+        function = function,
+        argsList = argsList,
         callableMethod = callableMethod,
         clazz = clazz
     )
