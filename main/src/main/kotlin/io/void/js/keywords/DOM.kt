2256f51cacc524c41e909966052cd2373145389f
Jadiefication <jadeczjade008@gmail.com>
2025-04-28 20:58:52 +0200

    chore: add more DOM features

--- main/src/main/kotlin/io/void/js/keywords/DOM.kt
+++ main/src/main/kotlin/io/void/js/keywords/DOM.kt
@@ -6,6 +6,7 @@ import io.void.html.attributes.Attribute
 import io.void.html.attributes.AttributeNames
 import io.void.js.JavaScript
 import io.void.js.keywords.datastructures.JsList
+import io.void.js.keywords.datastructures.JsObject
 import io.void.js.keywords.string.TemplateString
 import io.void.js.keywords.variable.Variable
 import java.lang.UnsupportedOperationException
@@ -41,6 +42,10 @@ class DOM(document: Variable<DOM>? = null): BrowserObject {
         }
         if (elementValue.attributes.isNotEmpty()) attributes.replaceAfterLast(",", "")
         jsReturn = "elements(${amount.toJs()}, \"${elementValue.name}\", \"${elementValue.children!!.joinToString("") { it.render() }}\", {$attributes})"
+        return this
+    }
+    fun elements(amount: JsValue<Int>, element: JsValue<HTMLElement>, elementInsides: JsValue<String>, attribute: JsValue<JsObject>): BrowserObject {
+        jsReturn = "elements(${amount.toJs()}, ${element}, ${elementInsides}, $attribute)"
         return this
     }
 
@@ -84,6 +89,73 @@ class HTMLElement: BrowserObject {
     fun attribute(attributeName: JsValue<String>, attributeValue: JsValue<String>): Reference<HTMLElement>  {
         jsReturn += ".setAttribute($attributeName, $attributeValue)"
         return this.refer()
+    }
+    fun attributes(element: JsValue<HTMLElement>, attrbitues: JsValue<JsObject>): Reference<HTMLElement> {
+        jsReturn = "attributes($element, $attrbitues)"
+        return this.refer()
+    }
+    fun parent(onlyElements: Boolean): HTMLElement {
+        jsReturn += ".parent${if (onlyElements) "Element" else "Node"}"
+        return HTMLElement()
+    }
+    fun children(): JsList<HTMLElement> {
+        jsReturn += ".children"
+        return JsList(HTMLElement().asJsValue())
+    }
+    fun sibling(next: Boolean): HTMLElement {
+        jsReturn += ".${if (next) "next" else "previous"}Sibling"
+        return HTMLElement()
+    }
+    fun closest(id: JsValue<String>): HTMLElement {
+        jsReturn += ".closest($id)"
+        return HTMLElement()
+    }
+    fun matches(id: JsValue<String>): JsValue<Boolean> {
+        jsReturn += ".matches($id)"
+        return true.asJsValue()
+    }
+    fun getRect(): JsObject {
+        jsReturn += ".getBoundingClientRect()"
+        return JsObject(mapOf())
+    }
+    fun getOffset(): JsObject {
+        jsReturn += ".offset()"
+        return JsObject(mapOf())
+    }
+    fun getDimensions(): JsObject {
+        jsReturn += ".getBoundingClientRect()"
+        return JsObject(mapOf())
+    }
+    fun style(id: JsValue<String>, value: JsValue<String>): Reference<HTMLElement> {
+        jsReturn += ".style.$id = $value"
+        return this.refer()
+    }
+    fun style(id: JsValue<String>): JsValue<String> {
+        jsReturn += ".style.$id"
+        return "".asJsValue()
+    }
+    fun selectAll(identifier: JsValue<String>): JsList<HTMLElement> {
+        jsReturn += ".querySelectorAll(${identifier.toJs()})"
+        return JsList(HTMLElement().asJsValue())
+    }
+    fun before(child: JsValue<HTMLElement>, element: JsValue<HTMLElement>): HTMLElement {
+        jsReturn += ".insertBefore($child, $element)"
+        return HTMLElement()
+    }
+    fun replace(child: JsValue<HTMLElement>, element: JsValue<HTMLElement>): HTMLElement {
+        jsReturn += ".replaceChild($child, $element)"
+        return HTMLElement()
+    }
+    fun remove() {
+        jsReturn += ".remove()"
+    }
+    fun remove(element: JsValue<HTMLElement>): HTMLElement {
+        jsReturn += ".removeChild($element)"
+        return HTMLElement()
+    }
+    fun add(element: JsValue<HTMLElement>): HTMLElement {
+        jsReturn += ".appendChild($element)"
+        return HTMLElement()
     }
 }
 
