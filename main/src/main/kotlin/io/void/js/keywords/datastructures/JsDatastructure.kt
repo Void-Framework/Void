336f4ab5cb166b5ed8797fcd2e2c448cfcf9f441
Jadiefication <jadeczjade008@gmail.com>
2025-05-01 17:49:12 +0200

    refactor!: remove Runnable

--- main/src/main/kotlin/io/void/js/keywords/datastructures/JsDatastructure.kt
+++ main/src/main/kotlin/io/void/js/keywords/datastructures/JsDatastructure.kt
@@ -2,24 +2,24 @@ package io.void.js.keywords.datastructures
 
 import io.void.js.data.DataHandler
 import io.void.js.Function
+import io.void.js.FunctionVariable
+import io.void.js.JavaScript
 import io.void.js.data.randomString
 import io.void.js.keywords.Keyword
+import io.void.js.keywords.Lambda
+import io.void.js.keywords.Reference
 import io.void.js.keywords.asJsValue
+import io.void.js.keywords.refer
 
 interface JsDatastructure: Keyword {
 
     fun initialize(): JsDatastructure
 
-    fun forEach(): Runnable {
-        jsReturn += ".forEach("
-        return Runnable(this)
-    }
-}
-
-class Runnable(var keyword: Keyword) {
-    fun <T> run(function: Function<T>): Runnable {
-        val random = String.randomString(5)
-        keyword.jsReturn += "$random => ${function.run(function.getArg(random).asJsValue())})"
-        return this
+    fun forEach(body: JavaScript.(List<FunctionVariable<*>>) -> Unit): Reference<JsDatastructure> {
+        jsReturn += ".forEach(${Lambda<Nothing>(
+            _arguments = listOf(String.randomString(4)),
+            _body = body
+        ).render()})"
+        return this.refer()
     }
 }
