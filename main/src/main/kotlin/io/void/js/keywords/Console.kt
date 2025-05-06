2e1378216f819cb0049fc7177457b8df436faee6
Jadiefication <jadeczjade008@gmail.com>
2025-05-06 22:04:53 +0200

    feat: finish Console

--- main/src/main/kotlin/io/void/js/keywords/Console.kt
+++ main/src/main/kotlin/io/void/js/keywords/Console.kt
@@ -24,18 +24,65 @@ class Console(console: JsValue<*>? = null): Keyword {
         jsReturn += ".log($message)"
         return this.refer()
     }
-    fun table(list: JsValue<*>): Reference<Console> {
-        jsReturn += ".table($list)"
+    fun table(list: JsValue<*>, collums: JsValue<Int>? = null): Reference<Console> {
+        jsReturn += ".table($list${if (collums != null) ", $collums" else ""})"
         return this.refer()
     }
     fun warn(message: JsValue<*>): Reference<Console> {
         jsReturn += ".warn($message)"
         return this.refer()
     }
+    fun time(label: JsValue<String>? = null): Reference<Console> {
+        jsReturn += ".time(${if (label != null) "$label" else ""})"
+        return this.refer()
+    }
+    fun timeEnd(label: JsValue<String>? = null): Reference<Console> {
+        jsReturn += ".timeEnd(${if (label != null) "$label" else ""})"
+        return this.refer()
+    }
+    fun timeLog(label: JsValue<String>? = null): Reference<Console> {
+        jsReturn += ".timeLog(${if (label != null) "$label" else ""})"
+        return this.refer()
+    }
 }
 
-fun JavaScript.console(): Console {
-    val console = Console()
+fun JavaScript.log(message: JsValue<*>, console: JsValue<*>? = null) {
+    val console = Console(console)
     children.add(console)
-    return console
+    console.log(message)
+}
+fun JavaScript.error(message: JsValue<*>, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.error(message)
+}
+fun JavaScript.info(message: JsValue<*>, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.info(message)
+}
+fun JavaScript.table(list: JsValue<*>, collums: JsValue<Int>? = null, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.table(list, collums)
+}
+fun JavaScript.warn(message: JsValue<*>, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.warn(message)
+}
+fun JavaScript.time(label: JsValue<String>? = null, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.time(label)
+}
+fun JavaScript.timeEnd(label: JsValue<String>? = null, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.timeEnd(label)
+}
+fun JavaScript.timeLog(label: JsValue<String>? = null, console: JsValue<*>? = null) {
+    val console = Console(console)
+    children.add(console)
+    console.timeLog(label)
 }
