93148f2e80063d9bda6876963f74db1ad17d89fb
Jadiefication <jadeczjade008@gmail.com>
2025-05-09 17:02:36 +0200

    chore: make empty returns

--- main/src/main/kotlin/io/void/js/keywords/Return.kt
+++ main/src/main/kotlin/io/void/js/keywords/Return.kt
@@ -9,10 +9,20 @@ data class Return<T>(val value: T?): Keyword {
     override fun render(): String {
         return jsReturn
     }
+    fun emptyReturn(): Return<T> {
+        jsReturn = "return;"
+        return this
+    }
 }
 
 inline fun <reified T> JavaScript.Return(_value: T?): Return<T> {
     val Return = Return(value = _value)
     children.add(Return)
+    return Return
+}
+fun JavaScript.Return(): Return<Nothing> {
+    val Return = Return(value = null)
+    children.add(Return)
+    Return.emptyReturn()
     return Return
 }
