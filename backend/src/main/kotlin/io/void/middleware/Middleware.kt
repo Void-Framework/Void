918b99aa96a41ce986c334eeec15086081427b39
jadie <jadeczjade008@gmail.com>
2025-09-22 21:13:04 +0200

    chore: simplify Middleware.kt

--- backend/src/main/kotlin/io/void/middleware/Middleware.kt
+++ backend/src/main/kotlin/io/void/middleware/Middleware.kt
@@ -12,3 +12,18 @@ interface Middleware {
 
     fun handleError(e: Exception): ResponseDTO?
 }
+
+fun middleware(priority: Int = 0, block: MiddlewareBuilder.() -> Unit): Middleware =
+    object : Middleware {
+        override val priority = priority
+        private val builder = MiddlewareBuilder().apply(block)
+        override fun processBefore(requestDTO: RequestDTO) = builder.before(requestDTO)
+        override fun processAfter(requestDTO: RequestDTO) = builder.after(requestDTO)
+        override fun handleError(e: Exception) = builder.onError(e)
+    }
+
+class MiddlewareBuilder {
+    var before: (RequestDTO) -> ResponseDTO? = { null }
+    var after: (RequestDTO) -> ResponseDTO? = { null }
+    var onError: (Exception) -> ResponseDTO? = { null }
+}
