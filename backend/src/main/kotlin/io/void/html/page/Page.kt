f575b63812ed7d99e1dcd13b9afb06db2a50830b
jadie <jadeczjade008@gmail.com>
2025-09-22 21:06:04 +0200

    feat: add easier way to define pages

--- backend/src/main/kotlin/io/void/html/page/Page.kt
+++ backend/src/main/kotlin/io/void/html/page/Page.kt
@@ -1,9 +1,11 @@
 package io.void.html.page
 
 import io.void.dto.http.RequestDTO
+import io.void.dto.http.ResponseDTO
 import io.void.html.Element
 import io.void.html.page.content.ContentType
 import io.void.html.page.metadata.Metadata
+import io.void.html.page.metadata.metadata
 import kotlin.reflect.KClass
 
 abstract class Page<T : ContentType>(
@@ -17,3 +19,19 @@ abstract class Page<T : ContentType>(
 
     abstract fun content(): T
 }
+
+fun htmlRoute(path: String, metadata: Metadata.() -> Unit, block: ContentType.HtmlElements.() -> Unit): Page<ContentType.HtmlElements> =
+    object : Page<ContentType.HtmlElements>(target = path) {
+        private val _metadata = metadata(this) {  }.apply(metadata)
+        override var metadata: Metadata? = _metadata
+        override val contentType = ContentType.HtmlElements::class
+        override fun content() = ContentType.HtmlElements().apply(block)
+    }
+
+fun jsonRoute(path: String, block: () -> ResponseDTO): Page<ContentType.Response> =
+    object : Page<ContentType.Response>(target = path) {
+        override var metadata: Metadata? = null
+        override val contentType = ContentType.Response::class
+        override fun content() = ContentType.Response(block())
+    }
+
--- backend/src/main/kotlin/io/void/html/page/content/ContentType.kt
+++ backend/src/main/kotlin/io/void/html/page/content/ContentType.kt
@@ -2,13 +2,16 @@ package io.void.html.page.content
 
 import io.void.dto.http.ResponseDTO
 import io.void.html.Element
+import io.void.html.page.metadata.Metadata
 
 sealed class ContentType {
     class HtmlElements internal constructor() : ContentType() {
         lateinit var htmlElement: Element
+        lateinit var metadata: Metadata
 
-        constructor(element: Element) : this() {
+        constructor(element: Element, metadata: Metadata) : this() {
             htmlElement = element
+            this@HtmlElements.metadata = metadata
         }
     }
 
