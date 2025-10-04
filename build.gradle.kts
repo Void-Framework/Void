f86f8eb6d3e7ddd20bfe91c409e2ca802d7014af
jadie <jadeczjade008@gmail.com>
2025-10-04 23:09:15 +0200

    chore: fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,11 @@ tasks.withType<JavaCompile> {
 tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenKotlin") {
+            from(components["kotlin"])
+        }
+    }
+}
