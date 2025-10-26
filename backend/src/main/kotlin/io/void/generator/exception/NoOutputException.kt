8d4337fd235157817c3029ea99d85e9905ef25f3
jadie <jadeczjade008@gmail.com>
2025-10-26 21:30:32 +0100

    chore: fifth iteration of KDocs

--- backend/src/main/kotlin/io/void/generator/exception/NoOutputException.kt
+++ backend/src/main/kotlin/io/void/generator/exception/NoOutputException.kt
@@ -1,3 +1,9 @@
 package io.void.generator.exception
 
+/**
+ * Thrown when a required output directory/path has not been configured for the generator.
+ *
+ * Indicates that code attempted to write generated assets without a destination. Configure
+ * an output path before invoking the generator to avoid this exception.
+ */
 class NoOutputException : Exception("No output directory was specified.")
