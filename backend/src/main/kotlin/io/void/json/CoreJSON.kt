91004bebe2fb4e56c5ef44dc8916067463569226
jadie <jadeczjade008@gmail.com>
2025-10-31 17:52:07 +0100

    feat: util functions

--- backend/src/main/kotlin/io/void/json/CoreJSON.kt
+++ backend/src/main/kotlin/io/void/json/CoreJSON.kt
@@ -1,16 +1,23 @@
 package io.void.json
 
+import io.void.cache.Cacheable
 import io.void.dto.http.RequestDTO
 import io.void.dto.http.ResponseDTO
 import io.void.dto.http.buildResponse
 import io.void.html.page.Page
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonBuilder
 import kotlinx.serialization.protobuf.ProtoBuf
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Base64
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.findAnnotations
 
 object JsonConfigs {
     val default = Json {
@@ -29,7 +36,7 @@ inline fun <reified T : Any> T.toJson(pretty: Boolean = false): Result<String> =
     val json = if (pretty) JsonConfigs.pretty else JsonConfigs.default
     json.encodeToString(this)
 }
-inline fun <reified T : Any> String.fromJson(pretty: Boolean = false): Result<T> = runCatching { Json.decodeFromString(this) }
+inline fun <reified T : Any> String.fromJson(): Result<T> = runCatching { Json.decodeFromString(this) }
 
 @OptIn(ExperimentalSerializationApi::class)
 inline fun <reified T : Any> T.toBytes(): Result<ByteArray> = runCatching { Cbor.encodeToByteArray(this) }
@@ -37,9 +44,9 @@ inline fun <reified T : Any> T.toBytes(): Result<ByteArray> = runCatching { Cbor
 inline fun <reified T : Any> ByteArray.fromJson(): Result<T> = runCatching { Cbor.decodeFromByteArray(this) }
 
 @OptIn(ExperimentalSerializationApi::class)
-inline fun <reified T : Any> T.toXml(pretty: Boolean = false): Result<ByteArray> = runCatching { ProtoBuf.encodeToByteArray(this) }
+inline fun <reified T : Any> T.toXml(): Result<ByteArray> = runCatching { ProtoBuf.encodeToByteArray(this) }
 @OptIn(ExperimentalSerializationApi::class)
-inline fun <reified T : Any> ByteArray.fromXml(pretty: Boolean = false): Result<T> = runCatching { ProtoBuf.decodeFromByteArray(this) }
+inline fun <reified T : Any> ByteArray.fromXml(): Result<T> = runCatching { ProtoBuf.decodeFromByteArray(this) }
 
 inline fun <reified T> RequestDTO.parseBody(): Result<T> = runCatching { Json.decodeFromString(this.body) }
 
@@ -64,3 +71,15 @@ fun Page<*>.autoSerialize(value: Any): ResponseDTO {
         this.body = body
     }
 }
+
+inline fun <reified T : Any> T.toJsonFile(pretty: Boolean = false, path: Path) {
+    Files.createFile(path)
+    Files.write(path, this.toJson(pretty).getOrNull()!!.toByteArray())
+}
+inline fun <reified T : Any> File.fromJsonFile(): Result<T> = this.readText().fromJson()
+
+inline fun <reified T : Any> T.toJson64(): Result<String> = runCatching { Base64.getEncoder().encodeToString(this.toJson().getOrNull()!!.toByteArray()) }
+inline fun <reified T : Any> String.fromJson64(): Result<T> = String(Base64.getDecoder().decode(this)).fromJson()
+
+inline fun <reified T : Any> T.canSerialize(): Boolean = this::class.findAnnotation<Cacheable>() != null
+
