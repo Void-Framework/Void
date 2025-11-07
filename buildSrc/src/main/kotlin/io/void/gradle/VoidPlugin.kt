abbcffaecd3192748f8526e29fc11c15ce244a5b
jadie <jadeczjade008@gmail.com>
2025-11-07 13:15:26 +0100

    fix: fix the plugin

--- buildSrc/src/main/kotlin/io/void/gradle/VoidPlugin.kt
+++ buildSrc/src/main/kotlin/io/void/gradle/VoidPlugin.kt
@@ -4,10 +4,15 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.tooling.GradleConnector
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+import java.io.File
 import java.nio.file.FileSystems
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardWatchEventKinds
+import java.nio.file.WatchService
 import kotlin.concurrent.thread
+import kotlin.io.path.absolute
+import kotlin.io.path.extension
 
 class VoidPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -25,35 +30,35 @@ class VoidPlugin : Plugin<Project> {
 
                 val watcher = FileSystems.getDefault().newWatchService()
                 sourceDirs.forEach { dir ->
-                    println("👀 Watching: ${dir.absolutePath}")
-                    dir.toPath().register(
-                        watcher,
-                        StandardWatchEventKinds.ENTRY_MODIFY,
-                        StandardWatchEventKinds.ENTRY_CREATE,
-                        StandardWatchEventKinds.ENTRY_DELETE
-                    )
+                    dir.toPath().registerAll(watcher)
                 }
 
                 thread {
                     while (true) {
                         val key = watcher.take()
                         key.pollEvents().forEach { e ->
-                            val file = e.context() as Path
-                            if (file.toString().endsWith(".kt")) {
-                                println("💡 Changed: $file")
+                            val changedFile = (key.watchable() as Path).resolve(e.context() as Path)
+                            if (changedFile.extension == "kt") {
+                                println("💡 Changed: $changedFile")
 
-                                GradleConnector.newConnector()
-                                    .forProjectDirectory(project.projectDir)
-                                    .connect().use {
-                                        it.newBuild().forTasks("compileKotlin").run()
-                                    }
+                                ProcessBuilder("gradle", "compileKotlin").inheritIO().start()
                             }
                         }
                         key.reset()
                     }
                 }
-                Thread.sleep(Long.MAX_VALUE)
+                Thread.currentThread().join()
             }
         }
     }
+
+    fun Path.registerAll(watcher: WatchService) {
+        Files.walk(this).filter { Files.isDirectory(it) }.forEach {
+            println("👀 Watching: ${it.absolute()}")
+            it.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY)
+        }
+    }
+
 }
