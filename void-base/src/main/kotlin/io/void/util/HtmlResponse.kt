b8a7b8c82ccfd1cafe115beb050b68895f2f52f8
jadie <jadeczjade008@gmail.com>
2025-11-11 21:36:04 +0100

    chore: create a util function

---
+++ void-base/src/main/kotlin/io/void/util/HtmlResponse.kt
@@ -0,0 +1,18 @@
+package io.void.util
+
+import io.void.dto.http.ResponseDTO
+import io.void.dto.http.buildResponse
+import io.void.dto.http.headers
+import io.void.dto.http.ok
+import io.void.html.Element
+import io.void.html.page.metadata.Metadata
+
+fun createResponse(element: Element, metadata: Metadata): ResponseDTO = ok("""
+                        <!doctype html><html>
+                        <head>${metadata.render()}</head>
+                        <body>${element.render()}</body>
+                        </html>
+                        """.trimIndent(), mutableMapOf("Content-Type" to "text/html"))
+
+
+fun createResponse(element: Element): ResponseDTO = ok(element.render().trimIndent(), mutableMapOf("Content-Type" to "text/html"))
