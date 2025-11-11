77b016ad30322bd66377124883e495e39b953fa2
jadie <jadeczjade008@gmail.com>
2025-11-11 21:46:40 +0100

    refactor!: move the rest of the pages to new standard

--- void-base/src/main/kotlin/io/void/api/JsPage.kt
+++ void-base/src/main/kotlin/io/void/api/JsPage.kt
@@ -1,10 +1,10 @@
 package io.void.api
 
 import io.void.api.method.Method
+import io.void.dto.http.ResponseDTO
 import io.void.dto.http.buildResponse
 import io.void.dto.http.headers
 import io.void.html.page.Page
-import io.void.html.page.content.ContentType
 import io.void.html.page.metadata.Metadata
 import io.void.html.page.metadata.metadata
 import java.util.*
@@ -17,36 +17,33 @@ import kotlin.reflect.KClass
 internal class JsPage(
     uuid: UUID,
     private val body: String,
-) : Page<ContentType.Response>(
+) : Page(
         target = "/js/$uuid/script.js",
     ) {
-    override val contentType: KClass<ContentType.Response> = ContentType.Response::class
     override var metadata: Metadata? = null
 
-    override fun content(): ContentType.Response =
-        ContentType.Response(
-            if (request.method == Method.GET) {
-                buildResponse {
-                    status = 200
-                    statusText = "All is Well"
-                    headers {
-                        put("Content-Type", "text/javascript")
-                        put("X-Content-Type-Options", "nosniff")
-                    }
-                    body = this@JsPage.body
+    override fun content(): ResponseDTO =
+        if (request.method == Method.GET) {
+            buildResponse {
+                status = 200
+                statusText = "All is Well"
+                headers {
+                    put("Content-Type", "text/javascript")
+                    put("X-Content-Type-Options", "nosniff")
                 }
-            } else {
-                buildResponse<String> {
-                    status = 405
-                    statusText = "Method not allowed"
-                }
-            },
-        )
+                body = this@JsPage.body
+            }
+        } else {
+            buildResponse<String> {
+                status = 405
+                statusText = "Method not allowed"
+            }
+        }
 
     /** Utility functions to attach the JS page into HTML page metadata. */
     companion object {
         fun addToMetadata(
-            page: Page<ContentType.HtmlElements>,
+            page: Page,
             jsPage: JsPage,
         ) {
             if (page.metadata == null) {
@@ -64,7 +61,7 @@ internal class JsPage(
         }
 
         fun addToMetadata(
-            page: Page<ContentType.HtmlElements>,
+            page: Page,
             jsPage: List<JsPage>,
         ) {
             jsPage.forEach {
