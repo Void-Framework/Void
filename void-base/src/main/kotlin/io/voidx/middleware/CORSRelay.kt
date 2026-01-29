ed3667959dbccc372ed095253d9703cc8f0589cd
jadie <jadeczjade008@gmail.com>
2026-01-29 20:59:58 +0100

    feat: cors relay

---
+++ void-base/src/main/kotlin/io/voidx/middleware/CORSRelay.kt
@@ -0,0 +1,59 @@
+package io.voidx.middleware
+
+import io.voidx.Method
+import io.voidx.dto.buildResponse
+import io.voidx.page.Page
+
+fun Page.corsMiddleware(allowedOrigins: Set<String>? = null) {
+    before(
+        relayBefore { pReq ->
+            val req = pReq.getOrThrow()
+            val origin = req["Origin"]
+            val allowOrigin =
+                when {
+                    allowedOrigins == null -> "*"
+                    origin != null && origin in allowedOrigins -> origin
+                    else -> null
+                }
+
+            if (req.method == Method.OPTIONS) {
+                val headers = mutableMapOf<String, String>()
+                if (allowOrigin != null) {
+                    headers["Access-Control-Allow-Origin"] = allowOrigin
+                    headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+                    headers["Access-Control-Allow-Headers"] = "*"
+                    headers["Access-Control-Allow-Credentials"] = "true"
+                }
+                return@relayBefore buildResponse {
+                    status = 200
+                    statusText = "OK"
+                    body = ""
+                    this.headers = headers
+                }
+            }
+
+            null // continue normal request
+        },
+    )
+
+    // attach headers after normal request
+    after(
+        relayAfter { pResp ->
+            val resp = pResp.getOrThrow()
+            val origin = resp.request["Origin"]
+            val allowOrigin =
+                when {
+                    allowedOrigins == null -> "*"
+                    origin != null && origin in allowedOrigins -> origin
+                    else -> null
+                }
+
+            if (allowOrigin != null) {
+                resp.headers["Access-Control-Allow-Origin"] = allowOrigin
+                resp.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+                resp.headers["Access-Control-Allow-Headers"] = "*"
+                resp.headers["Access-Control-Allow-Credentials"] = "true"
+            }
+        },
+    )
+}
