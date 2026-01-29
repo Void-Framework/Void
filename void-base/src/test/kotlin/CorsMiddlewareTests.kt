c7c6aad29e7d81f1b551bf38a794e4dca9e878dd
jadie <jadeczjade008@gmail.com>
2026-01-29 21:23:01 +0100

    chore: ktlint

--- void-base/src/test/kotlin/CorsMiddlewareTests.kt
+++ void-base/src/test/kotlin/CorsMiddlewareTests.kt
@@ -4,28 +4,29 @@ import io.voidx.Method
 import io.voidx.dto.buildRequest
 import io.voidx.dto.buildResponse
 import io.voidx.dto.ok
+import io.voidx.middleware.corsMiddleware
 import io.voidx.page.Page
 import io.voidx.page.route
-import io.voidx.middleware.corsMiddleware
 import io.voidx.router.router
 import io.voidx.util.toResult
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class CorsMiddlewareTests {
-
     @Test
     fun `OPTIONS preflight is short circuited`() {
-        val page = route("/test") {
-            corsMiddleware()
-        }
+        val page =
+            route("/test") {
+                corsMiddleware()
+            }
 
-        page.request = buildRequest {
-            method = Method.OPTIONS
-            headers["Origin"] = "http://example.com"
-        }
+        page.request =
+            buildRequest {
+                method = Method.OPTIONS
+                headers["Origin"] = "http://example.com"
+            }
 
         val resp = page.middlewareProcessBefore()
         resp?.let { page.middlewareProcessAfter(resp.toResult()) }
@@ -39,15 +40,17 @@ class CorsMiddlewareTests {
 
     @Test
     fun `normal request continues to route`() {
-        val page = route("/test") {
-            corsMiddleware()
-            ok("Hello")
-        }
+        val page =
+            route("/test") {
+                corsMiddleware()
+                ok("Hello")
+            }
 
-        page.request = buildRequest {
-            method = Method.GET
-            headers["Origin"] = "http://example.com"
-        }
+        page.request =
+            buildRequest {
+                method = Method.GET
+                headers["Origin"] = "http://example.com"
+            }
 
         val resp = page.middlewareProcessBefore()
         resp?.let { page.middlewareProcessAfter(resp.toResult()) }
@@ -57,32 +60,36 @@ class CorsMiddlewareTests {
     @Test
     fun `origin whitelist works`() {
         val allowed = setOf("https://allowed.com")
-        val page = route("/test") {
-            corsMiddleware(allowed)
-            ok("ok")
-        }
+        val page =
+            route("/test") {
+                corsMiddleware(allowed)
+                ok("ok")
+            }
 
         // allowed origin
-        page.request = buildRequest {
-            method = Method.GET
-            headers["Origin"] = "https://allowed.com"
-        }
+        page.request =
+            buildRequest {
+                method = Method.GET
+                headers["Origin"] = "https://allowed.com"
+            }
 
         val resp = page.content()
         // allowed origin
-        resp._request = buildRequest {
-            method = Method.GET
-            headers["Origin"] = "https://allowed.com"
-        }
+        resp._request =
+            buildRequest {
+                method = Method.GET
+                headers["Origin"] = "https://allowed.com"
+            }
         resp.let { page.middlewareProcessAfter(resp.toResult()) }
         assertEquals("https://allowed.com", resp.headers["Access-Control-Allow-Origin"])
 
         val resp2 = page.content()
         // disallowed origin
-        resp2._request = buildRequest {
-            method = Method.GET
-            headers["Origin"] = "https://notallowed.com"
-        }
+        resp2._request =
+            buildRequest {
+                method = Method.GET
+                headers["Origin"] = "https://notallowed.com"
+            }
         resp2.let { page.middlewareProcessAfter(it.toResult()) }
         assertNull(resp2.headers["Access-Control-Allow-Origin"]) // no header
     }
