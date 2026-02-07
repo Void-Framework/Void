71e0efe2bbb506f5c4d4c860357b3d1210e7fa8e
jadie <jadeczjade008@gmail.com>
2026-02-07 19:32:30 +0100

    refactor: make it automatically usable

--- void-base/src/main/kotlin/io/voidx/page/PageHandler.kt
+++ void-base/src/main/kotlin/io/voidx/page/PageHandler.kt
@@ -4,6 +4,7 @@ import io.voidx.Method
 import io.voidx.dto.RequestDTO
 import io.voidx.dto.ResponseDTO
 import io.voidx.dto.emptyResponse
+import io.voidx.json.Negotiator
 
 /**
  * Lightweight page that dispatches to verb-specific handlers for API-style routes.
@@ -15,35 +16,35 @@ class PageHandler(
 ) : DynamicPage(
         target = target,
     ) {
-    val responses = mutableMapOf<Method, (RequestDTO) -> ResponseDTO>()
+    val responses = mutableMapOf<Method, Negotiator.() -> ResponseDTO>()
 
     /** Returns the response from the registered handler for [RequestDTO.method], or an empty response if none. */
-    override fun content(): ResponseDTO = responses[request.method]?.invoke(request) ?: emptyResponse()
+    override fun content(): ResponseDTO = responses[request.method]?.invoke(Negotiator(request)) ?: emptyResponse()
 
     /** Registers a GET handler. */
-    infix fun GET(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.GET] = body }
+    infix fun GET(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.GET] = body }
 
     /** Registers a POST handler. */
-    infix fun POST(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.POST] = body }
+    infix fun POST(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.POST] = body }
 
     /** Registers a HEAD handler. */
-    infix fun HEAD(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.HEAD] = body }
+    infix fun HEAD(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.HEAD] = body }
 
     /** Registers a PUT handler. */
-    infix fun PUT(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.PUT] = body }
+    infix fun PUT(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.PUT] = body }
 
     /** Registers a DELETE handler. */
-    infix fun DELETE(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.DELETE] = body }
+    infix fun DELETE(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.DELETE] = body }
 
     /** Registers a CONNECT handler. */
-    infix fun CONNECT(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.CONNECT] = body }
+    infix fun CONNECT(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.CONNECT] = body }
 
     /** Registers an OPTIONS handler. */
-    infix fun OPTIONS(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.OPTIONS] = body }
+    infix fun OPTIONS(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.OPTIONS] = body }
 
     /** Registers a TRACE handler. */
-    infix fun TRACE(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.TRACE] = body }
+    infix fun TRACE(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.TRACE] = body }
 
     /** Registers a PATCH handler. */
-    infix fun PATCH(body: (RequestDTO) -> ResponseDTO): PageHandler = apply { responses[Method.PATCH] = body }
+    infix fun PATCH(body: Negotiator.() -> ResponseDTO): PageHandler = apply { responses[Method.PATCH] = body }
 }
